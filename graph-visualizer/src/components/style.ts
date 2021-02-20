bb13478a68948da63f883e28045746532a39dd28
ensj <ensjtrans@gmail.com>
2021-02-20 16:57:48 -0500

    initial cytoscape setup, thanks to kenjinp on codesandbox

---
+++ graph-visualizer/src/components/style.ts
@@ -0,0 +1,30 @@
+import { Stylesheet } from 'cytoscape'
+
+const COLORS = {
+  purp: '#43447a',
+}
+
+const nodeStyles = [
+  {
+    selector: 'node',
+    style: {
+      'transition-property': 'background-color border-color',
+      'transition-duration': '0.3s',
+      'transition-timing-function': 'ease-in-sine',
+      'background-color': COLORS.purp,
+    },
+  },
+]
+const edgeStyles = [
+  {
+    selector: 'edge',
+    style: {
+      'curve-style': 'bezier',
+      'target-arrow-shape': 'triangle',
+      'target-arrow-color': COLORS.purp,
+      'line-color': COLORS.purp,
+    },
+  },
+]
+
+export default [...nodeStyles, ...edgeStyles] as Stylesheet[]
