89a99c06f1cd39ba4bcd05fa6980c7db789f09c1
jfantab <johnlu1161@gmail.com>
2021-02-20 14:25:53 -0800

    Consolidate graph component and user interface

--- graph-visualizer/src/App.tsx
+++ graph-visualizer/src/App.tsx
@@ -1,3 +1,5 @@
+import './App.css'
+
 import React from 'react'
 
 import GraphContainer from './containers/GraphContainer'
@@ -5,7 +7,6 @@ import GraphContainer from './containers/GraphContainer'
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <GraphContainer />
       <nav className="flex-row">
         <h1>Graph Visualizer</h1>
         <ul className="flex-row">
@@ -16,7 +17,7 @@ const App: React.FunctionComponent = () => {
       </nav>
       <div className="main flex-row">
         <section className="visual">
-          <div></div>
+          <GraphContainer />
           <svg width="28" height="31" viewBox="0 0 38 31" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M5.42857 19.9286H0V31H13.5714V26.5714H5.42857V19.9286ZM0 11.0714H5.42857V4.42857H13.5714V0H0V11.0714ZM32.5714 26.5714H24.4286V31H38V19.9286H32.5714V26.5714ZM24.4286 0V4.42857H32.5714V11.0714H38V0H24.4286Z"
