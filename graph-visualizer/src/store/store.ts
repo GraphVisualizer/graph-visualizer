e4bda54cd3bd8e83ace480c83a286a8aa4a338d3
ensj <ensjtrans@gmail.com>
2021-04-15 11:36:03 -0700

    revamped layout

--- graph-visualizer/src/store/store.ts
+++ graph-visualizer/src/store/store.ts
@@ -1,9 +1,12 @@
 import cytoscape from 'cytoscape'
+import cola from 'cytoscape-cola'
 import { v4 as uuidv4 } from 'uuid'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function createStore() {
-  return {
+  cytoscape.use(cola)
+
+  const store = {
     graph: cytoscape({
       style: [
         {
@@ -11,9 +14,32 @@ export function createStore() {
           style: { content: 'data(id)' },
         },
       ],
+      elements: [
+        // list of graph elements to start with
+        {
+          // node a
+          data: { id: 'a' },
+        },
+        {
+          // node b
+          data: { id: 'b' },
+        },
+        {
+          // edge ab
+          data: { id: 'ab', source: 'a', target: 'b' },
+        },
+      ],
       maxZoom: 1,
       wheelSensitivity: 0.2,
     }),
+
+    layout: {} as cytoscape.Layouts,
+    createLayout(options: cytoscape.LayoutOptions) {
+      this.layout = this.graph.layout(options)
+    },
+    refreshLayout() {
+      this.layout.run()
+    },
     addNode(item: string) {
       this.graph.add({ data: { id: item } })
     },
@@ -21,6 +47,13 @@ export function createStore() {
       this.graph.add({ data: { id: uuidv4(), source: e1, target: e2 } })
     },
   }
+
+  store.createLayout({
+    name: 'cola',
+  })
+  store.refreshLayout()
+
+  return store
 }
 
 export type TStore = ReturnType<typeof createStore>
