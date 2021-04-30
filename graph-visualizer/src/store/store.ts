70f47f57f0d66ee0b2d0895f66091326b4a7acd7
ensj <ensjtrans@gmail.com>
2021-04-30 14:35:49 -0700

    style addition

--- graph-visualizer/src/store/store.ts
+++ graph-visualizer/src/store/store.ts
@@ -2,18 +2,15 @@ import cytoscape from 'cytoscape'
 import cola from 'cytoscape-cola'
 import { v4 as uuidv4 } from 'uuid'
 
+import defaultStyle from './style'
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function createStore() {
   cytoscape.use(cola)
 
   const store = {
     graph: cytoscape({
-      style: [
-        {
-          selector: 'node',
-          style: { content: 'data(id)' },
-        },
-      ],
+      style: defaultStyle,
       elements: [
         // list of graph elements to start with
         {
@@ -45,6 +42,14 @@ export function createStore() {
     },
     addEdge(e1: string, e2: string) {
       this.graph.add({ data: { id: uuidv4(), source: e1, target: e2 } })
+    },
+    bfs() {
+      this.graph.elements().bfs({
+        roots: `#elemActions.selected`,
+        visit: (v, e, u, i, depth) => {
+          setTimeout(() => v.addClass('alg'), 1000 * depth)
+        },
+      })
     },
   }
 
