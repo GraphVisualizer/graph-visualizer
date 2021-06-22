93977cfac3f1fbbf3aacc5559e9eaa89f8534f9d
ensj <ensjtrans@gmail.com>
2021-06-21 17:58:46 -0700

    combine molecule and organism, and also set up algorithm to prep for erasing graph

--- graph-visualizer/src/components/organisms/AdjacencyImportForm/index.tsx
+++ graph-visualizer/src/components/organisms/AdjacencyImportForm/index.tsx
@@ -4,12 +4,14 @@ import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { useDataStore } from '../../../store/context'
-import TextAreaWithButton from '../../molecules/TextAreaWithButton'
+import Button from '../../atoms/Button'
+import TextArea from '../../atoms/TextArea'
 
 const App: React.FunctionComponent = () => {
   const store = useDataStore()
 
   const [formValue, changeFormValue] = useState<string>('')
+  const [textVal, changeTextVal] = useState<string>('')
 
   useEffect(() => {
     if (formValue === '') return
@@ -22,19 +24,26 @@ const App: React.FunctionComponent = () => {
       const edges = Number(initValues[1])
 
       const nodeList = new Map<string, string>()
+      const edgeList: Array<[string, string]> = []
 
       // for now, ignore the node names
       const nodeNames = lines[1].split(' ')
       for (let i = 0; i < nodes; i += 1) {
         const name = uuidv4()
         nodeList.set(nodeNames[i], name)
-        store.addNode(name)
       }
 
       for (let i = 0; i < edges; i += 1) {
         const edge = lines[2 + i].split(' ')
-        store.addEdge(nodeList.get(edge[0]) as string, nodeList.get(edge[1]) as string)
+        edgeList.push([edge[0], edge[1]])
       }
+
+      nodeList.forEach((val) => {
+        store.addNode(val)
+      })
+      edgeList.forEach((edge) => {
+        store.addEdge(nodeList.get(edge[0]) as string, nodeList.get(edge[1]) as string)
+      })
 
       // eslint-disable-next-line no-alert
       alert(`Graph of ${nodes} nodes and ${edges} edges successfully added.`)
@@ -48,7 +57,8 @@ const App: React.FunctionComponent = () => {
 
   return (
     <div>
-      <TextAreaWithButton onTextSubmit={changeFormValue} />
+      <TextArea value={textVal} onChange={changeTextVal} />
+      <Button onClick={() => changeFormValue(textVal)}>Submit</Button>
     </div>
   )
 }
