e8930becf79352849a3f5f8ed6d153b59ba7fe48
jfantab <johnlu1161@gmail.com>
2021-09-18 16:38:25 -0700

    Create initial redesign of tabs on right side

--- graph-visualizer/src/modules/App/index.tsx
+++ graph-visualizer/src/modules/App/index.tsx
@@ -36,7 +36,7 @@ const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <div className="main flex-row">
-        <div className="flex-column">
+        <div className="flex-column main-visual">
           <div className="flex-row fns">
             <Button onClick={() => resetGraph()}>Clear Graph</Button>
             <Button onClick={() => addNode(uuidv4())}>Add Node</Button>
@@ -56,58 +56,60 @@ const App: React.FunctionComponent = () => {
               <Button onClick={() => setIsAlgTabOn(true)}>Algorithms</Button>
             </span>
           </div>
-          {!isAlgTabOn ? (
-            <div className="page flex-column" id="presets">
-              <h4 className="button-subheading">Preset Graphs</h4>
-              <Button onClick={() => complete(5)}>
-                <h3>Complete n=5</h3>
-              </Button>
-              <Button onClick={() => star(10)}>
-                <h3>Star v=1</h3>
-              </Button>
-              <Button onClick={() => cycle(10)}>
-                <h3>Cycle v=10</h3>
-              </Button>
-              <Button onClick={() => completeBipartite(5, 3)}>
-                <h3>Bipartite m = 5 n = 3</h3>
-              </Button>
-              <Button onClick={() => wheel(10)}>
-                <h3>Wheel n=10</h3>
-              </Button>
-              <h4 className="button-subheading">Custom Graph</h4>
-              <AdjacencyImportForm />
-            </div>
-          ) : (
-            <div className={'page'} id="algorithms">
-              <div>
-                <Button onClick={() => clearGraphAlgs()}>
-                  <h3>Clear</h3>
+          <div className="buttons">
+            {!isAlgTabOn ? (
+              <div className="page flex-column" id="presets">
+                <h4 className="button-subheading">Preset Graphs</h4>
+                <Button onClick={() => complete(5)}>
+                  <h3>Complete n=5</h3>
                 </Button>
-                <h4 className="button-subheading">Minimum Spanning Tree</h4>
-                <Button onClick={() => kruskal()}>
-                  <h3>Kruskal</h3>
+                <Button onClick={() => star(10)}>
+                  <h3>Star v=1</h3>
                 </Button>
-                <Button onClick={() => prim()}>
-                  <h3>Prim</h3>
+                <Button onClick={() => cycle(10)}>
+                  <h3>Cycle v=10</h3>
                 </Button>
-                <h4 className="button-subheading">Connected Components</h4>
-                <Button onClick={() => cutVertices()}>
-                  <h3>Cut Vertices</h3>
+                <Button onClick={() => completeBipartite(5, 3)}>
+                  <h3>Bipartite m = 5 n = 3</h3>
                 </Button>
-                <h4 className="button-subheading">Search Algorithms</h4>
-                <Button onClick={() => bfs()}>
-                  <h3>Breadth First Search</h3>
+                <Button onClick={() => wheel(10)}>
+                  <h3>Wheel n=10</h3>
                 </Button>
-                <Button onClick={() => dfs()}>
-                  <h3>Depth First Search</h3>
-                </Button>
-                <h4 className="button-subheading">Shortest Path Algorithm</h4>
-                <Button onClick={() => dijkstra()}>
-                  <h3>Dijkstra</h3>
-                </Button>
+                <h4 className="button-subheading">Custom Graph</h4>
+                <AdjacencyImportForm />
               </div>
-            </div>
-          )}
+            ) : (
+              <div className={'page'} id="algorithms">
+                <div>
+                  <Button onClick={() => clearGraphAlgs()}>
+                    <h3>Clear</h3>
+                  </Button>
+                  <h4 className="button-subheading">Minimum Spanning Tree</h4>
+                  <Button onClick={() => kruskal()}>
+                    <h3>Kruskal</h3>
+                  </Button>
+                  <Button onClick={() => prim()}>
+                    <h3>Prim</h3>
+                  </Button>
+                  <h4 className="button-subheading">Connected Components</h4>
+                  <Button onClick={() => cutVertices()}>
+                    <h3>Cut Vertices</h3>
+                  </Button>
+                  <h4 className="button-subheading">Search Algorithms</h4>
+                  <Button onClick={() => bfs()}>
+                    <h3>Breadth First Search</h3>
+                  </Button>
+                  <Button onClick={() => dfs()}>
+                    <h3>Depth First Search</h3>
+                  </Button>
+                  <h4 className="button-subheading">Shortest Path Algorithm</h4>
+                  <Button onClick={() => dijkstra()}>
+                    <h3>Dijkstra</h3>
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
         </section>
       </div>
     </div>
