29e0c0cae8e2530fb08767e3007d20bf845db670
ensj <ensjtrans@gmail.com>
2021-09-30 12:32:04 -0700

    Contact info changes

--- graph-visualizer/src/modules/Contact/index.tsx
+++ graph-visualizer/src/modules/Contact/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import john from './img/john.jpeg'
-import junhyun from './img/junhyun.png'
+import junhyun from './img/junhyun.jpeg'
 import ContactTemplate from './template'
 
 const Contact: React.FunctionComponent = () => {
@@ -15,8 +15,7 @@ const Contact: React.FunctionComponent = () => {
         },
         {
           img: junhyun,
-          bio:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras scelerisque mollis velit ut malesuada. Donec volutpat leo ut odio aliquet molestie. Morbi tincidunt ante eu odio rutrum vulputate. Maecenas ac diam augue. Vivamus felis lacus, euismod eu enim placerat, consequat vulputate lectus. Nunc ut nulla vitae odio pretium pharetra. Aliquam posuere, lacus id rutrum tincidunt, nunc arcu posuere enim, non facilisis erat mi et massa. Sed vitae odio a arcu viverra placerat id sit amet dui. Vivamus et eleifend ligula, non cursus nisi. Ut commodo consectetur diam, a faucibus sapien interdum eu. Sed ut metus a justo tristique volutpat ornare in massa. In rutrum pharetra mi, quis aliquet mauris fringilla nec. Proin dignissim nibh pellentesque, sollicitudin diam eu, lobortis nisi. Donec fringilla, nulla in malesuada dapibus, magna dolor sagittis libero, auctor pharetra enim ex a nisi.',
+          bio: "Yo what's up. I'm Junhyun Lim, a Math & Computer Science graduate from Santa Clara University.",
         },
       ]}
     />
